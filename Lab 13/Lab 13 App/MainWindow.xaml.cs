a6753f0a78504268c73fca7b8eecaa5dcee804d3
Yekteniya2202 <yellowblue924@yandex.ru>
2021-11-11 14:06:48 +0300

    Lab 13 added

---
+++ Lab 13/Lab 13 App/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab_13_App
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CountriesDBDataSet _countiresDataGrid;
+        private CountriesDBDataSetTableAdapters.CountriesTableAdapter _countiresDataGridCountriesTableAdapter;
+        private CountriesDBDataSetTableAdapters.CitiesTableAdapter _countiresDataGridCitiesTableAdapter;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _countiresDataGrid = (CountriesDBDataSet)FindResource("CountriesDBDataSet");
+            // Загрузить данные в таблицу Countries. Можно изменить этот код как требуется.
+            _countiresDataGridCountriesTableAdapter = new CountriesDBDataSetTableAdapters.CountriesTableAdapter();
+            _countiresDataGridCountriesTableAdapter.Fill(_countiresDataGrid.Countries);
+            var usersViewSource = (CollectionViewSource)FindResource("countriesViewSource");
+            usersViewSource.View.MoveCurrentToFirst();
+            // Загрузить данные в таблицу Cities. Можно изменить этот код как требуется.
+            _countiresDataGridCitiesTableAdapter = new CountriesDBDataSetTableAdapters.CitiesTableAdapter();
+            _countiresDataGridCitiesTableAdapter.Fill(_countiresDataGrid.Cities);
+            var usersCitiesViewSource = (CollectionViewSource)FindResource("citiesViewSource");
+            usersCitiesViewSource.View.MoveCurrentToFirst();
+        }
+
+        private void DeleteCountry_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < countriesDataGrid.SelectedItems.Count; i++)
+            {
+                var dataRowView = countriesDataGrid.SelectedItems[i] as DataRowView;
+                if (dataRowView != null)
+                {
+                    DataRow dataRow = dataRowView.Row;
+                    dataRow.Delete();
+                }
+            }
+            _countiresDataGridCountriesTableAdapter.Update(_countiresDataGrid.Countries);
+        }
+
+        private void SaveCountry_Click(object sender, RoutedEventArgs e)
+        {
+            _countiresDataGridCountriesTableAdapter.Update(_countiresDataGrid.Countries);
+        }
+
+        private void SaveCity_Click(object sender, RoutedEventArgs e)
+        {
+            _countiresDataGridCitiesTableAdapter.Update(_countiresDataGrid.Cities);
+        }
+
+        private void DeleteCity_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < citiesDataGrid.SelectedItems.Count; i++)
+            {
+                var dataRowView = citiesDataGrid.SelectedItems[i] as DataRowView;
+                if (dataRowView != null)
+                {
+                    DataRow dataRow = dataRowView.Row;
+                    dataRow.Delete();
+                }
+            }
+            _countiresDataGridCitiesTableAdapter.Update(_countiresDataGrid.Cities);
+        }
+
+        private void LoadImage_OnClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                DefaultExt = ".jpg",
+                Filter = "Image files (*.jpg)|*.jpg|All files|*.*"
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                if (countriesDataGrid.SelectedItems.Count > 0)
+                {
+                    var dataRowView = countriesDataGrid.SelectedItems[0] as DataRowView;
+                    if (dataRowView != null)
+                    {
+                        var dataRow = dataRowView.Row as CountriesDBDataSet.CountriesRow;
+                        if (dataRow != null)
+                        {
+                            dataRow.Flag = File.ReadAllBytes(openFileDialog.FileName);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Lab 13/Lab13/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System.Data;
+using System.IO;
+using System.Windows;
+using System.Windows.Data;
+using Microsoft.Win32;
+
+namespace Lab13
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CommentDBDataSet _commentDbDataSet;
+        private CommentDBDataSetTableAdapters.UsersTableAdapter _commentDbDataSetUsersTableAdapter;
+        private CommentDBDataSetTableAdapters.CommentsTableAdapter _commentDbDataSetCommentsTableAdapter;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _commentDbDataSet = (CommentDBDataSet)FindResource("commentDBDataSet");
+            // Загрузить данные в таблицу Users. Можно изменить этот код как требуется.
+            _commentDbDataSetUsersTableAdapter = new CommentDBDataSetTableAdapters.UsersTableAdapter();
+            _commentDbDataSetUsersTableAdapter.Fill(_commentDbDataSet.Users);
+            var usersViewSource = (CollectionViewSource)FindResource("usersViewSource");
+            usersViewSource.View.MoveCurrentToFirst();
+            // Загрузить данные в таблицу Comments. Можно изменить этот код как требуется.
+            _commentDbDataSetCommentsTableAdapter = new CommentDBDataSetTableAdapters.CommentsTableAdapter();
+            _commentDbDataSetCommentsTableAdapter.Fill(_commentDbDataSet.Comments);
+            var usersCommentsViewSource = (CollectionViewSource)FindResource("commentsViewSource");
+            usersCommentsViewSource.View.MoveCurrentToFirst();
+        }
+
+        private void DeleteUser_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < usersDataGrid.SelectedItems.Count; i++)
+            {
+                var dataRowView = usersDataGrid.SelectedItems[i] as DataRowView;
+                if (dataRowView != null)
+                {
+                    DataRow dataRow = dataRowView.Row;
+                    dataRow.Delete();
+                }
+            }
+            _commentDbDataSetUsersTableAdapter.Update(_commentDbDataSet.Users);
+        }
+
+        private void SaveUser_Click(object sender, RoutedEventArgs e)
+        {
+            _commentDbDataSetUsersTableAdapter.Update(_commentDbDataSet.Users);
+        }
+
+        private void SaveComment_Click(object sender, RoutedEventArgs e)
+        {
+            _commentDbDataSetCommentsTableAdapter.Update(_commentDbDataSet.Comments);
+        }
+
+        private void DeleteComment_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < commentsDataGrid.SelectedItems.Count; i++)
+            {
+                var dataRowView = commentsDataGrid.SelectedItems[i] as DataRowView;
+                if (dataRowView != null)
+                {
+                    DataRow dataRow = dataRowView.Row;
+                    dataRow.Delete();
+                }
+            }
+            _commentDbDataSetCommentsTableAdapter.Update(_commentDbDataSet.Comments);
+        }
+
+        private void LoadImage_OnClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                DefaultExt = ".jpg",
+                Filter = "Image files (*.jpg)|*.jpg|All files|*.*"
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                if (usersDataGrid.SelectedItems.Count > 0)
+                {
+                    var dataRowView = usersDataGrid.SelectedItems[0] as DataRowView;
+                    if (dataRowView != null)
+                    {
+                        var dataRow = dataRowView.Row as CommentDBDataSet.UsersRow;
+                        if (dataRow != null)
+                        {
+                            dataRow.Photo = File.ReadAllBytes(openFileDialog.FileName);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
